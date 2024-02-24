f0519dd3c481b6dd615da313c7aba3b614bcf36b
Adeodato <jesus2001adeodato@gmail.com>
2024-02-24 00:41:30 -0600

    avance con lain

--- EncryptJs.js
+++ EncryptJs.js
@@ -1,5 +1,8 @@
-const elemento = document.getElementById('ExampleCanvas');
-const ctx = elemento.getContext('2d');
-ctx.beginPath();
-ctx.arc(200,200,190,0,2 * Math.PI)
-ctx.stroke();
+let valor_adqui;
+let new_parr = document.querySelector("#User_input");
+function buscar(elemento){
+    if(event.keyCode == 13) {
+        valor_adqui = elemento.value;
+        new_parr.innerHTML = ">User@WebPage: /Alura/Oracle$ " + valor_adqui;     
+    }
+}
