3b93c65a0f5330943bff8fe1ebc3596846290201
Adeodato <jesus2001adeodato@gmail.com>
2024-02-24 13:54:03 -0600

    Display

--- EncryptJs.js
+++ EncryptJs.js
@@ -1,16 +1,37 @@
 let valor_adqui;
-let new_parr = document.querySelector("#User_input");
+const arreglo = ["#inputOne","#inputTwo","#inputThree"];
+//let new_parr = document.querySelector(arreglo[0]);
 let default_oracle = ">User@WebPage: /Alura/Oracle$ "; 
+let ind =0; 
+//funcion display
+function disp(n){
+    
+    if(n!=arreglo.length){
+        document.querySelector(arreglo[n]).innerHTML = default_oracle;
+    }
+    
+    else if(n === arreglo.length){
+        ind =0; 
+        for(let j =0; j < arreglo.length; j++){
+            document.querySelector(arreglo[j]).innerHTML = ' ';
+        }
+    }
+    return 
+}
+//Función del enter 
 function buscar(elemento){
     if(event.keyCode == 13) {
         valor_adqui = elemento.value;
-        if(valor_adqui == 'start'){
-            new_parr.innerHTML = default_oracle + 'Empieza la encriptación';
-            
+        //inicio de los comandos 
+        if(valor_adqui == 'start' || 'Start'){
+            document.querySelector(arreglo[0]).innerHTML = default_oracle +'started';
+            ind++;
+            disp(ind);
         }
         else{
-            new_parr.innerHTML = default_oracle+ 'try again ';
+            new_parr.innerHTML = default_oracle +'Command not recognized';
         }
+        
         document.getElementById("myText").value ='';    
     }
 }
